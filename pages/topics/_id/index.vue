50009875683408a53d8ec40e777cabca05e1867a
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-02-01 05:38:42 +0800

    able to delete post

--- pages/topics/_id/index.vue
+++ pages/topics/_id/index.vue
@@ -12,25 +12,39 @@
           :key="index"
           class="ml-5 content"
         >
-          {{ content.body }}
+          <p>{{ content.body }}</p>
+          <div v-if="authenticated">
+            <div v-if="user.id === content.user.id">
+              <nuxt-link class="btn btn-info btn-sm" :to="{ name: 'topics-posts-edit', params: { id: content.id } }">Edit</nuxt-link>
+              <button @click="DeletePost(content.id)" class="btn btn-danger btn-sm">Delete</button>
+            </div>
+          </div>
           <p class="text-muted">
             {{ content.created_at }} by {{ content.user.name }}
           </p>
         </div>
-      </div> <!-- end of bg-light -->
+      </div>
+      <!-- end of bg-light -->
 
       <div class="mt-5 ml-5" v-if="authenticated">
         <form @submit.prevent="create">
           <div class="form-group">
             <label for="">Add New Post</label>
-            <textarea  v-model="body" class="form-control" placeholder="Write something..." rows="5"></textarea>
-            <small class="form-text text-danger" v-if="errors.body">{{ errors.body[0] }}</small>
-          </div>  
-          <button class="btn btn-outline-primary btn-sm" type="submit">Add A New Post</button>
+            <textarea
+              v-model="body"
+              class="form-control"
+              placeholder="Write something..."
+              rows="5"
+            ></textarea>
+            <small class="form-text text-danger" v-if="errors.body">{{
+              errors.body[0]
+            }}</small>
+          </div>
+          <button class="btn btn-outline-primary btn-sm" type="submit">
+            Add A New Post
+          </button>
         </form>
-
       </div>
-
     </div>
   </div>
 </template>
@@ -43,7 +57,7 @@ export default {
   data() {
     return {
       topic: "",
-      body: ''
+      body: ""
     };
   },
   async asyncData({ $axios, params }) {
@@ -52,12 +66,17 @@ export default {
   },
   methods: {
     async create() {
-        await this.$axios.$post(`/topics/${this.$route.params.id}/posts`, {body: this.body});
-        this.$router.push('/topics');
-     
+      try {
+        await this.$axios.$post(`/topics/${this.$route.params.id}/posts`, {
+          body: this.body
+        });
+        this.$router.push("/topics");
+      } catch (e) {}
     },
+    async DeletePost(id) {
+        await this.$axios.$delete(`/topics/${this.$route.params.id}/posts/${id}`);
+        this.$router.push('/topics');
+    }
   }
 };
 </script>
-
-<style></style>
---
+++ pages/topics/posts/edit/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <Navbar />
+      <div class="container">
+      <div class="row">
+          <div class="col-lg-12 mt-5">
+              <h2 class="h2 text-capitalize">Topic post edit page</h2>
+          </div>
+      </div>
+  </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar';
+export default {
+    components: { Navbar }
+}
+</script>
+
+<style>
+
+</style>
